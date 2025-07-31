3874e893a272d85d193fcb7a90a9195a5b87c082
Dev Dimma <elizabethndubuisi8@gmail.com>
2025-07-31 17:40:59 +0100

    first commit

---
+++ Quiz app/script.js
@@ -0,0 +1,38 @@
+const questions = [
+  {
+    question: "Which is the largest animal in the world",
+    answer: [
+      { text: "Shark", correct: false},
+      { text: "Blue whale", correct: true},
+      { text: "Elephant", correct: false},
+      { text: "Giraffe", correct: false},
+    ]
+  },
+  {
+     question: "Which is the smallest city in the world",
+    answer: [
+      { text: "Vatican city", correct: true},
+      { text: "Bhutan", correct: false},
+      { text: "Nepal", correct: false},
+      { text: "Shri Lanka", correct: false},
+    ]
+  },
+  {
+     question: "Which is the largest desert in the world",
+    answer: [
+      { text: "Kalahari", correct: false},
+      { text: "Gobi", correct: false},
+      { text: "Sahara", correct: false},
+      { text: "Antartica", correct: true},
+    ]
+  },
+  {
+     question: "Which is the smallest continent in the world",
+    answer: [
+      { text: "Asia", correct: false},
+      { text: "Australia", correct: true},
+      { text: "Arctic", correct: false},
+      { text: "Africa", correct: false},
+    ]
+  } 
+];
